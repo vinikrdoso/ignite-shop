46c921aabd78066c3609a921cb69feb2feae4d33
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-07-31 11:45:39 +0100

    feat: finish visual structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,58 @@
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
+import { useKeenSlider } from 'keen-slider/react'
+
+import camiseta1 from '../assets/camisetas/camiseta-1.png'
+import camiseta2 from '../assets/camisetas/camiseta-2.png'
+import camiseta3 from '../assets/camisetas/camiseta-3.png'
+import camiseta4 from '../assets/camisetas/camiseta-4.png'
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  });
+
   return (
-    <>
-      <p>oi</p>
-    </>
-  );
+   <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
